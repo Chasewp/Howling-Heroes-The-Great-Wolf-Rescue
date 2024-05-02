43d3961a4b5e36f0516160bd4d7d3c0edf5f8afb
Blizzard03 <mariq.rasyid@gmail.com>
2024-05-02 14:51:38 +0700

    update some code

---
+++ Assets/Script/Controller/UI/Exit_Game_Over/Exit_Game_Over.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@onready var Bye = $Bye
+@onready var gbye = $Good_Bye_Label
+#For sound
+func _ready():
+	Bye.play()
+
+
+func _on_bye_finished():
+	get_tree().quit()
---
+++ Assets/Script/Controller/UI/Game Over/Game_Over.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_quit_pressed():
+	get_tree().change_scene_to_file("res://Assets/Scences/UI/Exit_Game_Over/Exit_Game_Over.tscn")
+
+
+func _on_retry_pressed():
+	pass # Replace with function body.
