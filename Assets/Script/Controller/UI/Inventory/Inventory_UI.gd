35d84988fcd6d8f9d8d63e77a0a48727983cc27d
Blizzard03 <167230071+Chasewp@users.noreply.github.com>
2024-09-11 16:03:16 +0700

    reconfigure invontory menu

--- Assets/Script/Controller/UI/Inventory/Inventory_UI.gd
+++ Assets/Script/Controller/UI/Inventory/Inventory_UI.gd
@@ -28,25 +28,101 @@ func close() :
 func update(): 
 	for a in range(min(inventory.items.size(),slots.size())):
 		slots[a].update(inventory.items[a])
-		
-		
-		
-func _ready():
-	update()
-	print()
 
-
-
-func _on_inventory_panel_slot_mouse_entered():
-	lblname.set_text("Name :" + " " + inventory.items[0]["_name"])
-	lbldec.set_text("Descriptions :" + " " + inventory.items[0]["_description_Item"])
-	
-	
-
-	
 func _details_items_clear():
 	lblname.set_text("")
 	lbldec.set_text("")
 	
+func _ready():
+	update()
+	
+
+
+#Show Items Details
+func _on_inventory_panel_slot_mouse_entered():
+	lblname.set_text("Name :" + " " + inventory.items[0]["_name"])
+	lbldec.set_text("Descriptions :" + " " + inventory.items[0]["_description_Item"])
+
+func _on_inventory_panel_slot_2_mouse_entered():
+	lblname.set_text("Name :" + " " + inventory.items[1]["_name"])
+	lbldec.set_text("Descriptions :" + " " + inventory.items[1]["_description_Item"])
+
+func _on_inventory_panel_slot_3_mouse_entered():
+	lblname.set_text("Name :" + " " + inventory.items[2]["_name"])
+	lbldec.set_text("Descriptions :" + " " + inventory.items[2]["_description_Item"])
+
+func _on_inventory_panel_slot_4_mouse_entered():
+	lblname.set_text("Name :" + " " + inventory.items[3]["_name"])
+	lbldec.set_text("Descriptions :" + " " + inventory.items[3]["_description_Item"])
+
+func _on_inventory_panel_slot_5_mouse_entered():
+	lblname.set_text("Name :" + " " + inventory.items[4]["_name"])
+	lbldec.set_text("Descriptions :" + " " + inventory.items[4]["_description_Item"])
+
+func _on_inventory_panel_slot_6_mouse_entered():
+	lblname.set_text("Name :" + " " + inventory.items[5]["_name"])
+	lbldec.set_text("Descriptions :" + " " + inventory.items[5]["_description_Item"])
+
+func _on_inventory_panel_slot_7_mouse_entered():
+	lblname.set_text("Name :" + " " + inventory.items[6]["_name"])
+	lbldec.set_text("Descriptions :" + " " + inventory.items[6]["_description_Item"])
+
+func _on_inventory_panel_slot_8_mouse_entered():
+	lblname.set_text("Name :" + " " + inventory.items[7]["_name"])
+	lbldec.set_text("Descriptions :" + " " + inventory.items[7]["_description_Item"])
+
+func _on_inventory_panel_slot_9_mouse_entered():
+	lblname.set_text("Name :" + " " + inventory.items[8]["_name"])
+	lbldec.set_text("Descriptions :" + " " + inventory.items[8]["_description_Item"])
+
+func _on_inventory_panel_slot_10_mouse_entered():
+	lblname.set_text("Name :" + " " + inventory.items[9]["_name"])
+	lbldec.set_text("Descriptions :" + " " + inventory.items[9]["_description_Item"])
+
+func _on_inventory_panel_slot_11_mouse_entered():
+	lblname.set_text("Name :" + " " + inventory.items[10]["_name"])
+	lbldec.set_text("Descriptions :" + " " + inventory.items[10]["_description_Item"])
+
+func _on_inventory_panel_slot_12_mouse_entered():
+	lblname.set_text("Name :" + " " + inventory.items[11]["_name"])
+	lbldec.set_text("Descriptions :" + " " + inventory.items[11]["_description_Item"])
+
+#Erase the Data
 func _on_inventory_panel_slot_mouse_exited():
 	_details_items_clear()
+
+func _on_inventory_panel_slot_2_mouse_exited():
+	_details_items_clear()
+
+func _on_inventory_panel_slot_4_mouse_exited():
+	_details_items_clear()
+
+func _on_inventory_panel_slot_3_mouse_exited():
+	_details_items_clear()
+
+func _on_inventory_panel_slot_5_mouse_exited():
+	_details_items_clear()
+
+func _on_inventory_panel_slot_6_mouse_exited():
+	_details_items_clear()
+
+func _on_inventory_panel_slot_7_mouse_exited():
+	_details_items_clear()
+
+func _on_inventory_panel_slot_8_mouse_exited():
+	_details_items_clear()
+
+func _on_inventory_panel_slot_9_mouse_exited():
+	_details_items_clear()
+
+func _on_inventory_panel_slot_10_mouse_exited():
+	_details_items_clear()
+
+func _on_inventory_panel_slot_11_mouse_exited():
+	_details_items_clear()
+
+func _on_inventory_panel_slot_12_mouse_exited():
+	_details_items_clear()
+
+
+
