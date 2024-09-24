71356412d4c333c221347d0e0ea3a2dfd879871a
Blizzard Wolf Smith <167230071+Chasewp@users.noreply.github.com>
2024-09-24 21:27:11 +0700

    add some the code to test stage

--- Assets/Script/Controller/UI/Inventory/Inventory_Menu.gd
+++ Assets/Script/Controller/UI/Inventory/Inventory_Menu.gd
@@ -8,8 +8,8 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("open_inventory"):
 		if inventory_menu._isOpen:
-			get_tree().paused = false
+			#get_tree().paused = false
 			inventory_menu.close()
 		else:
 			inventory_menu.open()
-			get_tree().paused = true
+			#get_tree().paused = true
