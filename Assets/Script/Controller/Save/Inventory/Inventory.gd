83bf1017d49254b3f8e771260a2941677b87ecd4
CorneliusFox <99236016+CorneliusFox@users.noreply.github.com>
2024-09-30 10:29:00 +0700

    Add Inventory&StackingItem

--- Assets/Script/Controller/Save/Inventory/Inventory.gd
+++ Assets/Script/Controller/Save/Inventory/Inventory.gd
@@ -3,16 +3,36 @@ extends Resource
 
 signal updated
 
-@export var items : Array[InventoryItems]
+@export var slots : Array[InventorySlot]
 #@export var slots : Array[InventorySlot]
 
 func insert_items(item : InventoryItems):
-	for a in range(items.size()):
-		if !items[a]:
-			items[a] = item
-			break
+	var itemSlots = slots.filter(func(slot): return slot.item == item)
+	if !itemSlots.is_empty():
+		itemSlots[0]._quantity += 1
+	else:
+			var emptySlots = slots.filter(func(slot): return slot.item == null)
+			if !emptySlots.is_empty():
+				emptySlots[0].item = item
+				emptySlots[0]._quantity = 1
+				updated.emit()
+				
+				
+				
+	#for slot in slots:
+		#if slot.item == item:
+			#slot._quantity += 1
+			#updated.emit()
+			#return
 	
-	updated.emit()
+	#for a in range(slots.size()):
+		#if !slots[a].item:
+			#slots[a].item = item
+			#slots[a]._quantity = 1
+			#updated.emit()
+			#return
+	
+	
 #func insert(item: InventoryItems):
 	#var itemslots = slots.filter(
 		#func (slot):
--- Assets/Script/Controller/UI/Inventory/inventory_panel_slot.gd
+++ Assets/Script/Controller/UI/Inventory/inventory_panel_slot.gd
@@ -1,22 +1,24 @@
 extends Panel
 
 @onready var backgroundSprite : Sprite2D = $Background
-@onready var itemsspites : TextureRect = $Items
+@onready var itemssprites : TextureRect = $Items
 @onready var qty: Label = $Label
 
-func update(item : InventoryItems):
-		if!item:
+func update(slot : InventorySlot):
+		if!slot.item:
 			backgroundSprite.frame = 0
-			itemsspites.visible = false
+			itemssprites.visible = false
 			qty.visible = false
 		else:
 			backgroundSprite.frame = 1
-			itemsspites.visible = true
-			itemsspites.texture = item._texture
+			itemssprites.visible = true
+			itemssprites.texture = slot.item._texture
+			qty.visible = true
+			qty.text = str(slot._quantity)
 			
-			if item._qty>=1:
-				qty.visible = true
-				qty.set_text(str(item._qty))
-			else:
-				qty.visible = false
+			##if item._qty>=1:
+			##	qty.visible = true
+			##	qty.set_text(str(item._qty))
+			##else:
+				##qty.visible = false
 
